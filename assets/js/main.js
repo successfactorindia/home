6af42ecf88335255556230970ce8fd0657a481dc
Mohit Bhatia <mohitbhatia1994@gmail.com>
2020-08-29 14:01:22 -0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,26 +161,26 @@
   });
 
   // Gallery carousel (uses the Owl Carousel library)
-  $(".gallery-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    center: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 3
-      },
-      992: {
-        items: 4
-      },
-      1200: {
-        items: 5
-      }
-    }
-  });
+  // $(".gallery-carousel").owlCarousel({
+  //   autoplay: true,
+  //   dots: true,
+  //   loop: true,
+  //   center: true,
+  //   responsive: {
+  //     0: {
+  //       items: 1
+  //     },
+  //     768: {
+  //       items: 3
+  //     },
+  //     992: {
+  //       items: 4
+  //     },
+  //     1200: {
+  //       items: 5
+  //     }
+  //   }
+  // });
 
   // Buy tickets select the ticket type on click
   $('#buy-ticket-modal').on('show.bs.modal', function(event) {
